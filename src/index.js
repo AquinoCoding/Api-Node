a21bb3eef3d7d94fd3d08a46101d7c412d406815
LucasAquino09 <lucasaqs09@gmail.com>
2021-11-15 16:43:43 -0300

    Routes - Requests

--- src/index.js
+++ src/index.js
@@ -1,35 +1,18 @@
 const express = require('express');
-const insertBling = require('./controllers/insert')
-const pipeDriverApi = require('./controllers/pipedriver')
-const authConfig = require('./bin/auth.json')
+
+var blingRouter = require('./routes/bling');
+var pipeRouter = require('./routes/pipe');
 
 const app = express();
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(3000);
 console.log('http://localhost:3000/')
 
 
-app.get('/blind-insert', async (req, res) => {
+app.use('/pipe', pipeRouter);
+app.use('/bling', blingRouter);
 
-    try {
-        const {} = await insertBling.get(`produtos/json/&apikey=${authConfig.blingToken}`);
-        return res.send('OK')
-    }
-    catch (error) {
-        res.send({error: error.message})
-    }
-})
-
-app.get('/pipe-driver', async (req, res) => {
-
-    try {
-        const {} = await pipeDriverApi.get(`deals/?api_token=${authConfig.pipeToken}`);
-        return res.send('OK 2 ')
-    }
-    catch (error) {
-        res.send({error: error.message})
-    }
-})
-
+module.exports = app;
